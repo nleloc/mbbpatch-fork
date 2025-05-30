9ab54045091d8af99936fbe46ed30bbc5d1d1542
Cuynu <kongu2k@gmail.com>
2025-05-31 05:42:37 +0700

    typo fix

--- MBCPApp/patch.sh
+++ MBCPApp/patch.sh
@@ -248,7 +248,7 @@ fi
     rm -f 'mbapk/mbapk_unpacked/assets/policym'
     rm -f 'mbapk/mbapk_unpacked/assets/rulesV5.dat'
     rm -rf 'mbapk/mbapk_unpacked/smali/androidx/UnderlyingVcl'
-    rm -f 'mbapk/mbapk_unpacked/smali/UnderlyingVcl.smali'
+    rm -f 'mbapk/mbapk_unpacked/smali/androidx/UnderlyingVcl.smali'
     echo "Patching [AndroidManifest.xml]"
     sed -i 's|<provider android:authorities="com.mbmobile.honor.essence.enter" android:exported="false" android:name="androidx.cigarette.titles.corporation.moscow.Township"/>| |g' ~/mbbpatch/MBCPApp/mbapk/mbapk_unpacked/AndroidManifest.xml 
     sed -i 's|<provider android:authorities="com.mbmobile.inserted.thumbzilla.ireland" android:exported="false" android:name="androidx.UnderlyingVcl"/>| |g' ~/mbbpatch/MBCPApp/mbapk/mbapk_unpacked/AndroidManifest.xml
