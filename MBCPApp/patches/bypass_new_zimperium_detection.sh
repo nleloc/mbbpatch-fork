f612b72a7f6067fb8a15f0a4a16655d6bf855201
fukiame <fukiame@proton.me>
2025-08-05 18:40:16 +0700

    Revert "feat: bypass_new_zimperium_detection: also nuke libchecks.so"
    mbbpatch_chat/95193
      deleting libchecks.so on latest commit impact to app patching on my
      case btw. after comment the code all going good VTAP work
    
    This reverts commit 1005fd24f3fbfcca8e2e8fc461baf88d0158f5dd.
    
    Reported-by: chaadgrayson at Telegram
    Signed-off-by: Cuynu <cuynu@proton.me>
    Signed-off-by: fukiame <fukiame@proton.me>

--- MBCPApp/patches/bypass_new_zimperium_detection.sh
+++ MBCPApp/patches/bypass_new_zimperium_detection.sh
@@ -12,9 +12,6 @@ rm -f 'mbapk/mbapk_unpacked/lib/armeabi-v7a/libvvb2060.so'
 info 'Removing [libtoolChecker.so]...'
 rm -f 'mbapk/mbapk_unpacked/lib/arm64-v8a/libtoolChecker.so'
 rm -f 'mbapk/mbapk_unpacked/lib/armeabi-v7a/libtoolChecker.so'
-info 'Removing [libchecks.so]...'
-rm -f 'mbapk/mbapk_unpacked/lib/arm64-v8a/libchecks.so'
-rm -f 'mbapk/mbapk_unpacked/lib/armeabi-v7a/libchecks.so'
 info "Removing checksum from provider..."
 sed -i 's|e1a14adc915d7ad159edf2668b0dfcb359cf86538642de0e425d027f66eb07b2||g' "$DIRPATH"/mbapk/mbapk_unpacked/smali/androidx/UnderlyingVcl.smali
 sed -i 's|ca168a2ad00a92b8010d6801c3ca43e6df9f3701f084f6864399eaa4bbfaf56d||g' "$DIRPATH"/mbapk/mbapk_unpacked/smali/androidx/UnderlyingVcl.smali
