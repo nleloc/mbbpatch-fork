88b47e7afc9332d5853ad0499ae95d028fca2701
Cuynu <cuynu@proton.me>
2025-08-10 07:58:07 +0700

    feat: legacy_patch.sh: also remove unnecessary lib
    Signed-off-by: Cuynu <cuynu@proton.me>

--- MBCPApp/legacy_patch.sh
+++ MBCPApp/legacy_patch.sh
@@ -60,7 +60,12 @@ fi
     sed -i 's|ca168a2ad00a92b8010d6801c3ca43e6df9f3701f084f6864399eaa4bbfaf56d||g' "$DIRPATH"/mbapk/mbapk_unpacked/smali/androidx/UnderlyingVcl.smali
     sed -i 's|5c9a139e42e6e6032ddbc1092af9831b8142e4e5ae89700fc7828f5bd62e1671||g' "$DIRPATH"/mbapk/mbapk_unpacked/smali/androidx/UnderlyingVcl.smali
     sed -i 's|c0a161a71738083df4298a0f03b0c54abf5bfda97c7d230875d97593733226ec||g' "$DIRPATH"/mbapk/mbapk_unpacked/smali/androidx/UnderlyingVcl.smali
-    sed -i 's|designersactivists                                                                     |weneedfreedom|g' "$DIRPATH"/mbapk/mbapk_unpacked/smali/androidx/UnderlyingVcl.smali
+    sed -i 's|designersactivists                                                                     |weneedfreedom|g' "$DIRPATH"/mbapk/mbapk_unpacked/smali/androidx/UnderlyingVcl.smali    echo "Removing [libtoolChecker.so]..."
+    rm -f 'mbapk/mbapk_unpacked/lib/arm64-v8a/libtoolChecker.so'
+    rm -f 'mbapk/mbapk_unpacked/lib/armeabi-v7a/libtoolChecker.so'
+    echo "Removing [libvvb2060.so]..."
+    rm -f 'mbapk/mbapk_unpacked/lib/arm64-v8a/libvvb2060.so'
+    rm -f 'mbapk/mbapk_unpacked/lib/armeabi-v7a/libvvb2060.so'
     echo "Patching [libdesignersactivists.so]..."
     sed -i 's|com.zimperium|app.dontfixew|g' 'mbapk/mbapk_unpacked/lib/arm64-v8a/libdesignersactivists.so'
     sed -i 's|detection|freedom..|g' 'mbapk/mbapk_unpacked/lib/armeabi-v7a/libdesignersactivists.so'
