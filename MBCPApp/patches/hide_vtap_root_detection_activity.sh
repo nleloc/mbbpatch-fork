73d8a7e5986bf518933c973b894b6e91cea0f1d6
Cuynu <cuynu@proton.me>
2025-08-10 07:52:54 +0700

    feat: hide_vtap_root_detection_activity: adapt with v6.4.62+
    since v6.4.62+, its moved to smali_classes3
    
    Signed-off-by: Cuynu <cuynu@proton.me>

--- MBCPApp/patches/hide_vtap_root_detection_activity.sh
+++ MBCPApp/patches/hide_vtap_root_detection_activity.sh
@@ -17,5 +17,10 @@ sed -i 's|Lcom/vkey/android/vguard/VGDialogActivity;|Lio/flutter/plugins/MainAct
 cp -f 'blob_patches/bypass_rootold/BasicThreatInfo.smali' 'mbapk/mbapk_unpacked/smali_classes4/com/vkey/android/internal/vguard/engine/'
 cp -f 'blob_patches/bypass_rootold/VGThreatAppInfo.smali' 'mbapk/mbapk_unpacked/smali_classes4/com/vkey/android/vguard/model/'
 cp -f 'blob_patches/bypass_rootold/VGThreatResponse.smali' 'mbapk/mbapk_unpacked/smali_classes4/com/vkey/android/vguard/model/'
+# Adapt with v6.4.62+
+cp -f 'blob_patches/bypass_rootold/BasicThreatInfo.smali' 'mbapk/mbapk_unpacked/smali_classes3/com/vkey/android/internal/vguard/engine/'
+cp -f 'blob_patches/bypass_rootold/VGThreatAppInfo.smali' 'mbapk/mbapk_unpacked/smali_classes3/com/vkey/android/vguard/model/'
+cp -f 'blob_patches/bypass_rootold/VGThreatResponse.smali' 'mbapk/mbapk_unpacked/smali_classes3/com/vkey/android/vguard/model/'
 # Test crash fixes for some Android 13 devices
 sed -i 's|invoke-virtual {v5, v12}, Lcom/vkey/android/vguard/model/VGThreatResponse;->setUsingLegacyMessage(Z)V| |g' 'mbapk/mbapk_unpacked/smali_classes4/com/vkey/android/eg.smali'
+sed -i 's|invoke-virtual {v5, v12}, Lcom/vkey/android/vguard/model/VGThreatResponse;->setUsingLegacyMessage(Z)V| |g' 'mbapk/mbapk_unpacked/smali_classes3/com/vkey/android/eg.smali'
