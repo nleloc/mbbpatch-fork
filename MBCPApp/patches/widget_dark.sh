f0032c0db32a2368fd3a71b38f9a6282e407f771
Cuynu <cuynu@proton.me>
2025-08-11 16:19:45 +0700

    fix: widget_dark.sh: also remove manifest things
    fixes resource compilation error due to missing resources
    
    Signed-off-by: Cuynu <cuynu@proton.me>

--- MBCPApp/patches/widget_dark.sh
+++ MBCPApp/patches/widget_dark.sh
@@ -10,3 +10,7 @@ cp -f 'blob_patches/widget_dark/bg_4x2.png' 'mbapk/mbapk_unpacked/res/drawable-x
 cp -f 'blob_patches/widget_dark/bg_4x4.png' 'mbapk/mbapk_unpacked/res/drawable-xxhdpi/'
 echo "Removing [mbb_home_widget_qr_info.xml]..."
 rm -f 'mbapk/mbapk_unpacked/res/xml/mbb_home_widget_qr_info.xml'
+echo "Patching [public.xml]..."
+sed -i 's|<public type="xml" name="mbb_home_widget_qr_info" id="0x7f15000b" />||g' 'mbapk/mbapk_unpacked/res/values/public.xml'
+echo "Patching [AndroidManifest.xml]..."
+sed -i 's|<meta-data android:name="android.appwidget.provider" android:resource="@xml/mbb_home_widget_qr_info"/>||g' 'mbapk/mbapk_unpacked/AndroidManifest.xml'
