76909691eff93d345ae1a6064c976f26f08368ad
Cuynu <cuynu@proton.me>
2025-08-12 08:39:13 +0700

    feat: restore_old_lib_682: introduce [Restore old 682 lib] patch
    Signed-off-by: Cuynu <cuynu@proton.me>

---
+++ MBCPApp/patches/restore_old_lib_682.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# PATCHNAME: Restore old 682 lib 
+# MAXVER: 63
+# MINVER: 61
+# shellcheck disable=SC1091
+. "$DIRPATH"/common.sh
+
+mkdir old682
+echo "Downloading v6.4.61 apks..."
+wget https://github.com/kongu2k8-svg/mbapk/releases/download/mb/embee_v6.4.61_682_arm64-v8a_armeabi-v7a.apks -q --show-progress
+mv *.apks old682/
+echo "Unpacking..."
+java -jar tools/apkeditor.jar m -i old682/*.apks 
+java -jar tools/apktool.jar d old682/*.apk -o old682/unpacked -j$(nproc)
+mv old682/unpacked/lib/arm64-v8a/libapp.so mbapk/mbapk_unpacked/lib/arm64-v8a/ 
+mv old682/unpacked/lib/armeabi-v7a/libapp.so mbapk/mbapk_unpacked/lib/armeabi-v7a/ 
+mv old682/unpacked/lib/arm64-v8a/libflutter.so mbapk/mbapk_unpacked/lib/arm64-v8a/ 
+mv old682/unpacked/lib/armeabi-v7a/libflutter.so mbapk/mbapk_unpacked/lib/armeabi-v7a/   
+echo "Cleaning..."   
+rm -rf old682
+echo "WARNING : Any changes to [libapp.so] like Autopatch strings are reverted, please apply again if you want to !"
+echo "This patch are applied by MBCPApp Patcher on $(uname -s -r) with commit :" $(git rev-parse --short HEAD) at $(git show -s --format=%cd HEAD). > 'mbapk/mbapk_unpacked/assets/mbcp_info/bypass_1200_error.inf'
--- README.md
+++ README.md
@@ -33,6 +33,7 @@ We are not responsible for any illegal action if criminal abuse this patch to do
 | `Remove invoke to mbshield` | Make app don't invoke to mbshield when opening (MainActivity) and (MBBHomeWidgetQR) only | v6.4.22 ~ v6.4.62 |
 | `Remove bulit-in fonts` | Remove app font and force to use device font for most app components. | v6.4.0 ~ v6.4.62 |
 | `Modify app theme` | Modify the app theme in app from original one to others. Currently have MBClassic and SemiPriority. | v6.4.30 ~ v6.4.62 |
+| `Restore old 682 lib` | Restore old v6.4.61 `libapp.so` to prevent enforced `quockhanh` theme changes on v6.4.62. | v6.4.62 |
 | `Remove garbage permission and activities` | Removes tracking permission & activities from app. | v6.4.0 ~ v6.4.62 |
 | `Remove eMBee` | Removes eMBee options in app, alongside with Customer support. | v6.4.43 ~ v6.4.62 |
 | `Revert old eMBee logo` | Revert old eMBee logo, specially on product consultation options. | v6.4.53 ~ v6.4.62 |
