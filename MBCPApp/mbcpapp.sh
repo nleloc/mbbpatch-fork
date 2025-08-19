a89f55c41ffa69531d41ed7a3a0872d3d9fcedaa
Cuynu <cuynu@proton.me>
2025-08-19 17:04:52 +0700

    feat(deps): mbcpapp.sh: update APKEditor to 1.4.5
    Signed-off-by: Cuynu <cuynu@proton.me>

--- MBCPApp/mbcpapp.sh
+++ MBCPApp/mbcpapp.sh
@@ -55,7 +55,7 @@ Asset extraction are not needed :)'
 
 download_tools() {
     apktool_link="https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.12.0.jar"
-    apkeditor_link="https://github.com/REAndroid/APKEditor/releases/download/V1.4.4/APKEditor-1.4.4.jar"
+    apkeditor_link="https://github.com/REAndroid/APKEditor/releases/download/V1.4.5/APKEditor-1.4.5.jar"
 
     cd tools && rm -rf ./*.jar
     wget -q --show-progress -O apktool.jar "$apktool_link" || err "downloading apktool failed"
