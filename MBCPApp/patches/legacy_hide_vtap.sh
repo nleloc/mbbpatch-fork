9077ee7a35efa2db0312ab2ee0ebf9fd8ba3f0d7
Cuynu <cuynu@proton.me>
2025-08-29 20:46:05 +0700

    feat: legacy_hide_vtap.sh: introduce [Legacy hide VTAP activity & dialog] patch
    This patch is needed on some ugly devices that cannot work with standard [Hide VTAP root detection activity & dialog] patch and crashing
    
    It's behaviour is the same as before v6.4.53 : App opens but when trying to login, app will just simply throw GW908 error.
    
    Signed-off-by: Cuynu <cuynu@proton.me>

---
+++ MBCPApp/patches/legacy_hide_vtap.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# PATCHNAME: Legacy hide VTAP activity & dialog
+# MAXVER:
+# MINVER:
+# shellcheck disable=SC1091,SC2016
+. "$DIRPATH"/common.sh
+
+
+# Adapt with v6.4.64+
+
+warn "Use cases : Some devices that crashes with standard hide VTAP patch"
+warn "It is better to use the standard hide VTAP patch, so you don't have to hide anything"
+warn "This will only prevent the VGFullScreenDialogActivity from being shown, and the app inside will throw GW908 error when trying to signing in."
+warn "You have to hide root with some method for that, like freeze Magisk app with App Manager."
+info "Forcing flutter activity..."
+sed -i 's|const-class v1, Lcom/vkey/android/vguard/VGFullScreenDialogActivity;|const-class v1, Lio/flutter/plugins/MainActivity;|g' "$DIRPATH"/mbapk/mbapk_unpacked/smali_classes3/com/vkey/android/ep.smali && info "Done!"
+info "Removing VGFullScreenDialogActivity..."
+rm -f 'mbapk/mbapk_unpacked/smali_classes3/com/vkey/android/vguard/VGFullScreenDialogActivity.smali' && info "Done!"
+
+
