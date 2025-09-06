68ed020f6b6f0a41e5e431281584b287eea350de
Cuynu <cuynu@proton.me>
2025-09-07 05:45:25 +0700

    feat: remove_mbstore.sh: add remove mb themes store patch
    Signed-off-by: Cuynu <cuynu@proton.me>

---
+++ MBCPApp/patches/remove_mbstore.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# PATCHNAME: Remove MB Themes Store
+# MAXVER:
+# MINVER:
+# shellcheck disable=SC1091
+. "$DIRPATH"/common.sh
+
+warn "You won't be able to access MB Store Themes in the change theme section on the app !"
+info "Theme installation like specific theme modules that redirect to MB Store Themes will still work!"
+
+info "Removing MB Store Themes banner..."
+rm -f "mbapk/mbapk_unpacked/assets/flutter_assets/assets/images/dynamic/base/theme_bg_cuaHang.webp"
+rm -f "mbapk/mbapk_unpacked/assets/flutter_assets/assets/images/dynamic/base/theme_bg_cuaHang.private.webp"
+rm -f "mbapk/mbapk_unpacked/assets/flutter_assets/assets/images/dynamic/base/theme_img_cuaHang.private.webp"
+rm -f "mbapk/mbapk_unpacked/assets/flutter_assets/assets/images/dynamic/base/theme_img_cuaHang.webp"
+rm -f "mbapk/mbapk_unpacked/assets/flutter_assets/assets/images/dynamic/base/theme_img_headerCuaHang.webp"
+rm -f "mbapk/mbapk_unpacked/assets/flutter_assets/assets/images/dynamic/base/theme_img_headerCuaHang.private.webp"
+
+# Priority exclusive
+rm -f "mbapk/mbapk_unpacked/assets/flutter_assets/assets/images/dynamic/priority/theme_bg_cuaHang.webp"
+rm -f "mbapk/mbapk_unpacked/assets/flutter_assets/assets/images/dynamic/priority/theme_img_cuaHang.webp"
+rm -f "mbapk/mbapk_unpacked/assets/flutter_assets/assets/images/dynamic/priority/theme_img_headerCuaHang.webp"
