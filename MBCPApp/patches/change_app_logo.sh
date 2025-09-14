621da07a6e2b17054d9362e0295512c31ad22747
Cuynu <cuynu@proton.me>
2025-09-14 08:28:29 +0700

    feat: mbcpicons: new trungthu logo
    thanks to @minhwuangbeo (Telegram)
    
    Signed-off-by: Cuynu <cuynu@proton.me>

--- MBCPApp/patches/change_app_logo.sh
+++ MBCPApp/patches/change_app_logo.sh
@@ -6,7 +6,7 @@
 . "$DIRPATH"/common.sh
 
 PS3='Select app icon variant to continue : '
-select opt in 'MB Classic' 'Tet' 'Valentine 2025' 'SemiMonet' '30/4-1/5' '19/8-2/9 2025' 'Summer 2025' 'Noel' 'Exit'
+select opt in 'MB Classic' 'Tet' 'Valentine 2025' 'SemiMonet' '30/4-1/5' '19/8-2/9 2025' 'Summer 2025' 'Mid-autumn festival' 'Noel' 'Exit'
 do
     case "$opt" in
         'MB Classic') variant="normal" ;;
@@ -16,6 +16,7 @@ do
         '30/4-1/5') variant="thongnhatVN" ;;
 	'19/8-2/9 2025') variant="quockhanh2025" ;;
         'Summer 2025') variant="summer2025" ;;
+        'Mid-autumn festival') variant="trungthu" ;;
         'Noel') variant="noel" ;;
         'Exit') exit ;;
     esac
