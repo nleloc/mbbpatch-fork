87204effd9a57ce0b0ad21e3c6b7702a529e080f
Cuynu <cuynu@proton.me>
2025-10-05 11:51:01 +0700

    feat: spoof_appver.sh: add more app version
    Signed-off-by: Cuynu <cuynu@proton.me>

--- MBCPApp/patches/spoof_appver.sh
+++ MBCPApp/patches/spoof_appver.sh
@@ -7,5 +7,9 @@
 
 info "Spoofing app version..."
 sed -i 's|v6.4.66|v6.4.68|g' 'mbapk/mbapk_unpacked/AndroidManifest.xml'
-sed -i 's|696|698|g' 'mbapk/mbapk_unpacked/AndroidManifest.xml'
+sed -i 's|v6.4.65|v6.4.68|g' 'mbapk/mbapk_unpacked/AndroidManifest.xml'
+sed -i 's|v6.4.64|v6.4.68|g' 'mbapk/mbapk_unpacked/AndroidManifest.xml'
+sed -i 's|692|698|g' 'mbapk/mbapk_unpacked/AndroidManifest.xml'
+sed -i 's|691|698|g' 'mbapk/mbapk_unpacked/AndroidManifest.xml'
+sed -i 's|689|698|g' 'mbapk/mbapk_unpacked/AndroidManifest.xml'
 
