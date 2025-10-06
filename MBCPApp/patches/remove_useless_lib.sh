328a99e8a32b239936d6322f83db5103a76059de
Cuynu <cuynu@proton.me>
2025-10-07 02:54:39 +0700

    feat: remove_useless_lib.sh: introduce new forced patch
    Signed-off-by: Cuynu <cuynu@proton.me>

---
+++ MBCPApp/patches/remove_useless_lib.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# PATCHNAME: Remove useless libraries
+# MAXVER: 
+# MINVER: 68
+# shellcheck disable=SC1091
+. "$DIRPATH"/common.sh
+
+rm -rf 'mbapk/mbapk_unpacked/root/lib/arm64-v8a/libtoolChecker.so'
+rm -rf 'mbapk/mbapk_unpacked/root/lib/armeabi-v7a/libtoolChecker.so'
+
+rm -rf 'mbapk/mbapk_unpacked/root/lib/arm64-v8a/libvvb2060.so'
+rm -rf 'mbapk/mbapk_unpacked/root/lib/armeabi-v7a/libvvb2060.so'
