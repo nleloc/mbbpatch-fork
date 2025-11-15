48e0c95e35c708f7397b40bff8a6a1145052fb29
Cuynu <cuynu@proton.me>
2025-11-16 02:13:10 +0700

    feat: remove_mbai.sh: adapt with v6.4.75+
    starting with v6.4.75, MB now uses [mbai_img_search.webp] instead of [img_mbai_textLogo] as before
    
    this commit fixes empty search logo when patching with v6.4.75+.
    
    tested and working with v6.4.75.
    
    Signed-off-by: Cuynu <cuynu@proton.me>

--- MBCPApp/patches/remove_mbai.sh
+++ MBCPApp/patches/remove_mbai.sh
@@ -33,6 +33,9 @@ rm -rf 'mbapk/mbapk_unpacked/root/assets/flutter_assets/assets/images/static/bas
 # Replace MB AI search button with normal search button
 cp -f 'blob_patches/searchicon/search.png' 'mbapk/mbapk_unpacked/root/assets/flutter_assets/assets/images/dynamic/base/img_mbai_textLogo.webp'
 cp -f 'blob_patches/searchicon/search.png' 'mbapk/mbapk_unpacked/root/assets/flutter_assets/assets/images/dynamic/base/img_mbai_textLogo.private.webp'
+# Adapt new changes with v6.4.75
+cp -f 'blob_patches/searchicon/search.png' 'mbapk/mbapk_unpacked/root/assets/flutter_assets/assets/images/dynamic/base/mbai_img_search.webp'
+cp -f 'blob_patches/searchicon/search.png' 'mbapk/mbapk_unpacked/root/assets/flutter_assets/assets/images/dynamic/base/mbai_img_search.private.webp'
 
 
 
