e11051874b002052ef9eb37b11a0f831db3e5e58
fukiame <fukiame@proton.me>
2025-11-20 00:53:43 +0700

    feat: patches: introduce adaptive_ic_launcher
    adaptive launcher icon, with monochrome for actual "themed icon" support
    
    this "unfortunately" overrides all changes done if change_app_logo is applied
    
    Signed-off-by: fukiame <fukiame@proton.me>

---
+++ MBCPApp/patches/adaptive_ic_launcher.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# PATCHNAME: Adaptive launcher icon
+# MAXVER:
+# MINVER:
+# shellcheck disable=SC1091
+. "$DIRPATH"/common.sh
+
+confirm "This will override all changes in [Change app icon], are you sure you want to apply?" || exit 69
+
+res="$DIRPATH/mbapk/mbapk_unpacked/resources/package_1/res/"
+blob="$DIRPATH/blob_patches/adaptive_ic_launcher/"
+
+cp -r "$blob"/* "$res"
+
+sed -i '0,/type=.font/s|^.*type="font".*|  <public id="0x7f080365" type="drawable" name="ic_launcher_foreground" />\n&|' "$res"/values/public.xml
