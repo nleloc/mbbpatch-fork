622825733130f0dc17489cc8804b07cff621bb90
Cuynu <cuynu@proton.me>
2025-12-01 00:15:34 +0700

    fix: patches: remove_animated_qr_background: white QR button not applying with v6.4.77+
    MB v6.4.77 (711) switched to use [homeLanding_bg_qrCode.webp] instead of [ homeLanding_bg_qrCode.gif] as before
    
    this commit adapt with that new changes.
    
    Signed-off-by: Cuynu <cuynu@proton.me>

--- MBCPApp/patches/remove_animated_qr_background.sh
+++ MBCPApp/patches/remove_animated_qr_background.sh
@@ -7,7 +7,11 @@
 
 info "This will remove animated QR on bottom navigation bar and replace with white background !"
 info "Removing animated QR..."
-rm -f 'mbapk/mbapk_unpacked/root/assets/flutter_assets/assets/images/static/homeLanding_bg_qrCode.gif' 
+rm -f 'mbapk/mbapk_unpacked/root/assets/flutter_assets/assets/images/static/homeLanding_bg_qrCode.gif'
+# Adapt with v6.4.77+
+rm -f 'mbapk/mbapk_unpacked/root/assets/flutter_assets/assets/images/static/homeLanding_bg_qrCode.webp'
 info "Copying new static white background..."
-mv 'blob_patches/qr_white/150.png' 'blob_patches/qr_white/homeLanding_bg_qrCode.gif' > /dev/null 2>&1
 cp -f 'blob_patches/qr_white/homeLanding_bg_qrCode.gif' 'mbapk/mbapk_unpacked/root/assets/flutter_assets/assets/images/static'
+# Adapt with v6.4.77+
+cp -f 'blob_patches/qr_white/homeLanding_bg_qrCode.webp' 'mbapk/mbapk_unpacked/root/assets/flutter_assets/assets/images/static'
+
