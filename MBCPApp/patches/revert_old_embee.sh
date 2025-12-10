924109283668f578cbfd286057806d4552b0e2c2
Cuynu <cuynu@proton.me>
2025-12-11 00:08:56 +0700

    feat: patches: revert_old_embee.sh: also restore old bee rich logo
    new Bee Rich logo on MB looks just like testing logo
    
    the old one which is present before in the React Native version of MB (which is discontinued long time ago) is much better than the new one.
    
    Signed-off-by: Cuynu <cuynu@proton.me>

--- MBCPApp/patches/revert_old_embee.sh
+++ MBCPApp/patches/revert_old_embee.sh
@@ -27,3 +27,6 @@ cp -f 'blob_patches/revert_old_eMBee/homeLanding_img_avatarDefault.webp' 'mbapk/
 # Replace MB Junior avatars
 cp -f 'blob_patches/revert_old_eMBee/homeLanding_img_avatarDefault.webp' 'mbapk/mbapk_unpacked/root/assets/flutter_assets/assets/images/dynamic/base/junior_img_avatarBoy.webp'
 cp -f 'blob_patches/revert_old_eMBee/homeLanding_img_avatarDefault.webp' 'mbapk/mbapk_unpacked/root/assets/flutter_assets/assets/images/dynamic/base/junior_img_avatarGirl.webp'
+
+# Restore old Bee rich logo
+cp -f 'blob_patches/revert_old_eMBee/img_beerich.webp' 'mbapk/mbapk_unpacked/root/assets/flutter_assets/assets/images/static/base/partner/img_beerich.webp'
