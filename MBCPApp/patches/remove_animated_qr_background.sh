dbc04e741f28118d1d35b6ce9a49979b809d6bc4
Cuynu <cuynu@proton.me>
2025-12-17 23:23:35 +0700

    feat: patches: handle new homeLanding_bg_qrCode
    seems to exists since v6.4.77 or v6.4.78
    
    Signed-off-by: Cuynu <cuynu@proton.me>

--- MBCPApp/patches/remove_animated_qr_background.sh
+++ MBCPApp/patches/remove_animated_qr_background.sh
@@ -10,8 +10,11 @@ info "Removing animated QR..."
 rm -f 'mbapk/mbapk_unpacked/root/assets/flutter_assets/assets/images/static/homeLanding_bg_qrCode.gif'
 # Adapt with v6.4.77+
 rm -f 'mbapk/mbapk_unpacked/root/assets/flutter_assets/assets/images/static/homeLanding_bg_qrCode.webp'
+
 info "Copying new static white background..."
 cp -f 'blob_patches/qr_white/homeLanding_bg_qrCode.gif' 'mbapk/mbapk_unpacked/root/assets/flutter_assets/assets/images/static'
 # Adapt with v6.4.77+
 cp -f 'blob_patches/qr_white/homeLanding_bg_qrCode.webp' 'mbapk/mbapk_unpacked/root/assets/flutter_assets/assets/images/static'
+# Unknown homeLanding_bg_qrCode
+cp -f 'blob_patches/qr_white/homeLanding_bg_qrCode.webp' 'mbapk/mbapk_unpacked/root/assets/flutter_assets/assets/images/dynamic/homeLanding_bg_qrCode.webp'
 
