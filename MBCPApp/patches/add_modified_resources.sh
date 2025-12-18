57524fe5c0d2ddede26ac1c90d02c7315dd8fc03
Cuynu <cuynu@proton.me>
2025-12-19 01:32:08 +0700

    feat: patches: add_modified_resources.sh: also copying english strings
    Signed-off-by: Cuynu <cuynu@proton.me>

--- MBCPApp/patches/add_modified_resources.sh
+++ MBCPApp/patches/add_modified_resources.sh
@@ -8,3 +8,4 @@
 info "Copying modified resources..."
 cp -r -f 'blob_patches/resources/raw' 'mbapk/mbapk_unpacked/resources/package_1/res/'
 cp -f 'blob_patches/resources/strings-vi/strings.xml' 'mbapk/mbapk_unpacked/resources/package_1/res/values-vi/'
+cp -f 'blob_patches/resources/strings/strings.xml' 'mbapk/mbapk_unpacked/resources/package_1/res/values/'
