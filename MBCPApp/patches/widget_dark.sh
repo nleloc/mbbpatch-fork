e9434ae7178ceffc743125b561b5174c4b1c79b2
Cuynu <cuynu@proton.me>
2025-12-19 12:13:21 +0700

    feat: patches: widget_dark.sh: new font color & arrow button
    partially fixes display issues with MBBHomeWidget
    
    Signed-off-by: Cuynu <cuynu@proton.me>

--- MBCPApp/patches/widget_dark.sh
+++ MBCPApp/patches/widget_dark.sh
@@ -5,12 +5,15 @@
 # shellcheck disable=SC1091
 . "$DIRPATH"/common.sh
 
-echo "Copying dark background..."
+info "Copying dark background..."
 cp -f 'blob_patches/widget_dark/bg_4x2.png' 'mbapk/mbapk_unpacked/resources/package_1/res/drawable-xxhdpi/'
 cp -f 'blob_patches/widget_dark/bg_4x4.png' 'mbapk/mbapk_unpacked/resources/package_1/res/drawable-xxhdpi/'
-echo "Removing [mbb_home_widget_qr_info.xml]..."
+info "Removing [mbb_home_widget_qr_info.xml]..."
 rm -f 'mbapk/mbapk_unpacked/res/xml/mbb_home_widget_qr_info.xml'
-echo "Patching [public.xml]..."
+info "Patching [public.xml]..."
 sed -i 's|<public type="xml" name="mbb_home_widget_qr_info" id="0x7f15000b" />||g' 'mbapk/mbapk_unpacked/resources/package_1/res/values/public.xml'
-echo "Patching [AndroidManifest.xml]..."
+info "Patching [AndroidManifest.xml]..."
 sed -i 's|<meta-data android:name="android.appwidget.provider" android:resource="@xml/mbb_home_widget_qr_info"/>||g' 'mbapk/mbapk_unpacked/AndroidManifest.xml'
+info "Copying fixed widget files..."
+cp -f 'blob_patches/widget_dark/mbb_home_widget_4x4_all.xml' 'mbapk/mbapk_unpacked/resources/package_1/res/layout/'
+cp -f 'blob_patches/widget_dark/arrow.xml' 'mbapk/mbapk_unpacked/resources/package_1/res/drawable/'
