1d4814315353c6657142ba398f9a46de03ce4cb0
Cuynu <cuynu@proton.me>
2025-12-20 23:55:44 +0700

    fix: patches: changeapplogo.sh: proper logo year
    Signed-off-by: Cuynu <cuynu@proton.me>

--- MBCPApp/patches/change_app_logo.sh
+++ MBCPApp/patches/change_app_logo.sh
@@ -6,18 +6,18 @@
 . "$DIRPATH"/common.sh
 
 PS3='Select app icon variant to continue : '
-select opt in 'MB Classic' 'Tet' 'Valentine 2025' 'SemiMonet' '30/4-1/5' '19/8-2/9 2025' 'Summer 2025' 'Mid-autumn festival' 'Noel' 'Exit'
+select opt in 'MB Classic' 'New Year 2023' 'Valentine 2025' 'SemiMonet' '30/4-1/5' '19/8-2/9 2025' 'Summer 2025' 'Mid-autumn festival 2025' 'Noel 2024' 'Exit'
 do
     case "$opt" in
         'MB Classic') variant="normal" ;;
-        'Tet') variant="tet" ;;
+        'New Year 2023') variant="tet" ;;
         'Valentine 2025') variant="valentine" ;;
 	'SemiMonet') variant="semimonet" ;;
         '30/4-1/5') variant="thongnhatVN" ;;
 	'19/8-2/9 2025') variant="quockhanh2025" ;;
         'Summer 2025') variant="summer2025" ;;
-        'Mid-autumn festival') variant="trungthu" ;;
-        'Noel') variant="noel" ;;
+        'Mid-autumn festival 2025') variant="trungthu" ;;
+        'Noel 2024') variant="noel" ;;
         'Exit') exit ;;
     esac
 
