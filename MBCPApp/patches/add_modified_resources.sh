233d97792112096fd2b083f1bb99f54d9b23c715
Cuynu <cuynu@proton.me>
2026-01-17 22:19:39 +0700

    feat: add_modified_resources: bootleg ekyc guide
    Signed-off-by: Cuynu <cuynu@proton.me>

--- MBCPApp/patches/add_modified_resources.sh
+++ MBCPApp/patches/add_modified_resources.sh
@@ -9,3 +9,6 @@ info "Copying modified resources..."
 cp -r -f 'blob_patches/resources/raw' 'mbapk/mbapk_unpacked/resources/package_1/res/'
 cp -f 'blob_patches/resources/strings-vi/strings.xml' 'mbapk/mbapk_unpacked/resources/package_1/res/values-vi/'
 cp -f 'blob_patches/resources/strings/strings.xml' 'mbapk/mbapk_unpacked/resources/package_1/res/values/'
+info "Copying bootleg_ekyc..."
+cp -f 'blob_patches/bootleg_ekyc/dotp1_ekyc.png' 'mbapk/mbapk_unpacked/root/assets/flutter_assets/assets/images/dynamic/base/digitalOtp_img_authenguide01.webp'
+cp -f 'blob_patches/bootleg_ekyc/dotp2_ekyc.png' 'mbapk/mbapk_unpacked/root/assets/flutter_assets/assets/images/dynamic/base/digitalOtp_img_authenguide01.webp'
