510ad69a439db6b8e6495d494d6bd7315b9f57f1
Cuynu <cuynu@proton.me>
2026-01-22 00:10:51 +0700

    feat: custom_vtapscreen.sh: new `vtap` warning screen patch
    Signed-off-by: Cuynu <cuynu@proton.me>

---
+++ MBCPApp/patches/custom_vtapscreen.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# PATCHNAME: Custom VTAP screen 
+# MAXVER: 
+# MINVER: 52
+# shellcheck disable=SC1091
+. "$DIRPATH"/common.sh
+
+cp -f 'blob_patches/customvtap/custom_vtapdialog_activity.xml' 'mbapk/mbapk_unpacked/resources/package_1/res/layout/activity_vgfull_screen_dialog.xml'
