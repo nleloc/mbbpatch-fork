be57461cd8b4aada3671053a31797849d0ffc4cc
Cuynu <cuynu@proton.me>
2026-03-05 00:56:05 +0700

    fix: patches(spoof_appver): use "." instead of "_"
    fixes parse error after applying
    
    Signed-off-by: Cuynu <cuynu@proton.me>

--- MBCPApp/patches/spoof_appver.sh
+++ MBCPApp/patches/spoof_appver.sh
@@ -8,7 +8,7 @@
 info "Spoofing app version..."
 
 # Temp spoof only
-sed -i 's|6.4.90|6.4.90_mbcp|g' 'mbapk/mbapk_unpacked/AndroidManifest.xml'
+sed -i 's|6.4.90|6.4.90.mbcp|g' 'mbapk/mbapk_unpacked/AndroidManifest.xml'
 sed -i 's|727|2000|g' 'mbapk/mbapk_unpacked/AndroidManifest.xml'
 
 sed -i 's|v6.4.66|v6.4.68|g' 'mbapk/mbapk_unpacked/AndroidManifest.xml'
