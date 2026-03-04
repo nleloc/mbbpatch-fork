1c7ea4f99118f97ac1f515c447a311d7efb6456f
Cuynu <cuynu@proton.me>
2026-03-05 00:36:13 +0700

    fix: patches(remove_invoke_to_mbshield): both classes3 & 4 failling
    Signed-off-by: Cuynu <cuynu@proton.me>

--- MBCPApp/patches/remove_invoke_to_mbshield.sh
+++ MBCPApp/patches/remove_invoke_to_mbshield.sh
@@ -8,31 +8,30 @@
 warn "Currently only support remove invoke mbshield to [io.flutter.plugins.MainActivity] and [MBBHomeWidgetQR] !!!"
 info "Copying..."
 if [[ -f mbapk/mbapk_unpacked/smali/classes4/io/flutter/plugins/MainActivity.smali ]]; then
-	info "Flutter found on classes4 !!"
-	rm -f 'mbapk/mbapk_unpacked/smali/classes4/io/flutter/plugins/MainAcitivity.smali'
-	rm -f 'mbapk/mbapk_unpacked/smali/classes4/io/flutter/plugins/MBBHomeWidgetQR.smali'
-	cp -f 'blob_patches/noinvoke/MainActivity.smali' 'mbapk/mbapk_unpacked/smali/classes4/io/flutter/plugins'
-	cp -f 'blob_patches/noinvoke/MBBHomeWidgetQR.smali' 'mbapk/mbapk_unpacked/smali/classes4/io/flutter/plugins'
-	for i in {0..83}; do
-   case $i in
-       *) rm -f "mbapk/mbapk_unpacked/smali/classes5/mbshield/EP$i.smali" ;;
-   esac
-done
+  info "Flutter found on classes4 !!"
+  rm -f 'mbapk/mbapk_unpacked/smali/classes4/io/flutter/plugins/MainAcitivity.smali'
+  rm -f 'mbapk/mbapk_unpacked/smali/classes4/io/flutter/plugins/MBBHomeWidgetQR.smali'
+  cp -f 'blob_patches/noinvoke/MainActivity.smali' 'mbapk/mbapk_unpacked/smali/classes4/io/flutter/plugins'
+  cp -f 'blob_patches/noinvoke/MBBHomeWidgetQR.smali' 'mbapk/mbapk_unpacked/smali/classes4/io/flutter/plugins'
+  for i in {0..83}; do
+    case $i in
+    *) rm -f "mbapk/mbapk_unpacked/smali/classes5/mbshield/EP$i.smali" ;;
+    esac
+  done
 else
-	warn "Flutter not found on classes4, ignoring..."
+  warn "Flutter not found on classes4, ignoring..."
 fi
 
-	# Newer dex location (found on v6.4.82+, might present since v6.4.70+)
+# Newer dex location (found on v6.4.82+, might present since v6.4.70+)
 if [[ -f mbapk/mbapk_unpacked/smali/classes3/io/flutter/plugins/MainActivity.smali ]]; then
-	info "Flutter found on classes3 !!"
-	rm -f 'mbapk/mbapk_unpacked/smali/classes3/io/flutter/plugins/MainAcitivity.smali'
-	rm -f 'mbapk/mbapk_unpacked/smali/classes3/io/flutter/plugins/MBBHomeWidgetQR.smali'
-	cp -f 'blob_patches/noinvoke/MainActivity.smali' 'mbapk/mbapk_unpacked/smali/classes3/io/flutter/plugins'
-	cp -f 'blob_patches/noinvoke/MBBHomeWidgetQR.smali' 'mbapk/mbapk_unpacked/smali/classes3/io/flutter/plugins'
+  info "Flutter found on classes3 !!"
+  rm -f 'mbapk/mbapk_unpacked/smali/classes3/io/flutter/plugins/MainAcitivity.smali'
+  rm -f 'mbapk/mbapk_unpacked/smali/classes3/io/flutter/plugins/MBBHomeWidgetQR.smali'
+  cp -f 'blob_patches/noinvoke/MainActivity.smali' 'mbapk/mbapk_unpacked/smali/classes3/io/flutter/plugins'
+  cp -f 'blob_patches/noinvoke/MBBHomeWidgetQR.smali' 'mbapk/mbapk_unpacked/smali/classes3/io/flutter/plugins'
 else
-	err "Couldn't find Flutter on both classes3 & classes4! Aborting..."
+  warn "Couldn't find Flutter on both classes3 & classes4! Ignoring..."
 fi
 
-	info "Removing leftover files..."
+info "Removing leftover files..."
 # This is moved to classes4 but EP*.smali no longer exist since v6.4.70+
-
