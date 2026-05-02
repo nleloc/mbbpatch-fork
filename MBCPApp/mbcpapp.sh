11fc96dea0472f924c70726c61f0f1945d7bc6bd
Cuynu <cuynu@proton.me>
2026-05-02 22:25:06 +0700

    feat: patcher(mbcpapp.sh): warn for v6.4.98+
    Signed-off-by: Cuynu <cuynu@proton.me>

--- MBCPApp/mbcpapp.sh
+++ MBCPApp/mbcpapp.sh
@@ -299,6 +299,7 @@ do
     is_unpacked || exit 1
     isvcm1
     iszdefend
+    info "If you have v6.4.98 or newer, app resource extraction is no longer possible. Please use older version."
     info 'To extract encrypted assets [if current app has MBShield protection] you need rooted device'
     info 'And trigger a built-in app assets extraction !'
     echo -------------------------------------------
