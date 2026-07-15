badab4da08deb63fed43834d3f82d9d5e3b36294
nleloc <locleluanluc@gmail.com>
2026-07-15 22:30:33 +0700

    refactor (change_app_logo.sh): use [0] to exit

--- MBCPApp/patches/change_app_logo.sh
+++ MBCPApp/patches/change_app_logo.sh
@@ -5,34 +5,36 @@
 # shellcheck disable=SC1091
 . "$DIRPATH"/common.sh
 
-PS3='Select app icon variant to continue : '
-select opt in 'MB Classic' 'New Year 2023' 'Valentine 2025' 'SemiMonet' '30/4-1/5' '19/8-2/9 2025' 'Summer 2025' 'Mid-autumn festival 2025' 'Noel 2024' 'Exit'
-do
-    case "$opt" in
-        'MB Classic') variant="normal" ;;
-        'New Year 2023') variant="tet" ;;
-        'Valentine 2025') variant="valentine" ;;
-	'SemiMonet') variant="semimonet" ;;
-        '30/4-1/5') variant="thongnhatVN" ;;
-	'19/8-2/9 2025') variant="quockhanh2025" ;;
-        'Summer 2025') variant="summer2025" ;;
-        'Mid-autumn festival 2025') variant="trungthu" ;;
-        'Noel 2024') variant="noel" ;;
-        'Exit') exit ;;
-    esac
+PS3='Select app icon variant to continue, or [0] to exit : '
+select opt in 'MB Classic' 'New Year 2023' 'Valentine 2025' 'SemiMonet' '30/4-1/5' '19/8-2/9 2025' 'Summer 2025' 'Mid-autumn festival 2025' 'Noel 2024'; do
+  if [ "$REPLY" = "0" ]; then
+    exit
+  fi
 
-    info 'Removing old logos...'
-    rm -rf "$DIRPATH"/mbapk/mbapk_unpacked/resources/package_1/res/mipmap-hdpi
-    rm -rf "$DIRPATH"/mbapk/mbapk_unpacked/resources/package_1/res/mipmap-mdpi
-    rm -rf "$DIRPATH"/mbapk/mbapk_unpacked/resources/package_1/res/mipmap-xhdpi
-    rm -rf "$DIRPATH"/mbapk/mbapk_unpacked/resources/package_1/res/mipmap-xxhdpi
-    rm -rf "$DIRPATH"/mbapk/mbapk_unpacked/resources/package_1/res/mipmap-xxxhdpi
+  case "$opt" in
+  'MB Classic') variant="normal" ;;
+  'New Year 2023') variant="tet" ;;
+  'Valentine 2025') variant="valentine" ;;
+  'SemiMonet') variant="semimonet" ;;
+  '30/4-1/5') variant="thongnhatVN" ;;
+  '19/8-2/9 2025') variant="quockhanh2025" ;;
+  'Summer 2025') variant="summer2025" ;;
+  'Mid-autumn festival 2025') variant="trungthu" ;;
+  'Noel 2024') variant="noel" ;;
+  esac
 
-    info 'Copying new logos to [mbapk_unpacked/res]...'
-    cp -r -f "$DIRPATH"/mbcpicons/"$variant"/mipmap-hdpi mbapk/mbapk_unpacked/resources/package_1/res
-    cp -r -f "$DIRPATH"/mbcpicons/"$variant"/mipmap-mdpi mbapk/mbapk_unpacked/resources/package_1/res
-    cp -r -f "$DIRPATH"/mbcpicons/"$variant"/mipmap-xhdpi mbapk/mbapk_unpacked/resources/package_1/res
-    cp -r -f "$DIRPATH"/mbcpicons/"$variant"/mipmap-xxhdpi mbapk/mbapk_unpacked/resources/package_1/res
-    cp -r -f "$DIRPATH"/mbcpicons/"$variant"/mipmap-xxxhdpi mbapk/mbapk_unpacked/resources/package_1/res
+  info 'Removing old logos...'
+  rm -rf "$DIRPATH"/mbapk/mbapk_unpacked/resources/package_1/res/mipmap-hdpi
+  rm -rf "$DIRPATH"/mbapk/mbapk_unpacked/resources/package_1/res/mipmap-mdpi
+  rm -rf "$DIRPATH"/mbapk/mbapk_unpacked/resources/package_1/res/mipmap-xhdpi
+  rm -rf "$DIRPATH"/mbapk/mbapk_unpacked/resources/package_1/res/mipmap-xxhdpi
+  rm -rf "$DIRPATH"/mbapk/mbapk_unpacked/resources/package_1/res/mipmap-xxxhdpi
+
+  info 'Copying new logos to [mbapk_unpacked/res]...'
+  cp -r -f "$DIRPATH"/mbcpicons/"$variant"/mipmap-hdpi mbapk/mbapk_unpacked/resources/package_1/res
+  cp -r -f "$DIRPATH"/mbcpicons/"$variant"/mipmap-mdpi mbapk/mbapk_unpacked/resources/package_1/res
+  cp -r -f "$DIRPATH"/mbcpicons/"$variant"/mipmap-xhdpi mbapk/mbapk_unpacked/resources/package_1/res
+  cp -r -f "$DIRPATH"/mbcpicons/"$variant"/mipmap-xxhdpi mbapk/mbapk_unpacked/resources/package_1/res
+  cp -r -f "$DIRPATH"/mbcpicons/"$variant"/mipmap-xxxhdpi mbapk/mbapk_unpacked/resources/package_1/res
 
 done
